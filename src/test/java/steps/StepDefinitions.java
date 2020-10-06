6d876a8f243fdbc302f53d87901c0a1b00b40a94
Vita Kucheruk <vita.kucheruk@gmail.com>
2020-10-06 17:28:25 +0300

    hellocucumber

---
+++ src/test/java/steps/StepDefinitions.java
@@ -0,0 +1,13 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import pages.BasicPage;
+
+public class StepDefinitions {
+    BasicPage basicPage = new BasicPage ();
+
+    @Then("Click {string} button")
+    public void clickButton(String arg0) {
+        basicPage.clickButton (arg0);
+    }
+}
---
+++ src/test/resources/login.feature
@@ -0,0 +1,4 @@
+Feature: Login User
+
+  Scenario: Login user
+    Then Click "Log In" button
